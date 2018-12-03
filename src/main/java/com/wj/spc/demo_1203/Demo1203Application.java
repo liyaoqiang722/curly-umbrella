08e7d6753ac94a689e45f7c5203c214d016f6bed
liyaoqiang@wusongtech.com <wusongtech>
2018-12-03 15:34:23 +0800

    new project

---
+++ src/main/java/com/wj/spc/demo_1203/Demo1203Application.java
@@ -0,0 +1,12 @@
+package com.wj.spc.demo_1203;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1203Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1203Application.class, args);
+	}
+}
---
+++ src/test/java/com/wj/spc/demo_1203/Demo1203ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wj.spc.demo_1203;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo1203ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
